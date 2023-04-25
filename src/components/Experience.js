2e4766777b16ddf65927fc944145b120198ff8e2
jdabassett <jacobbassett@gmail.com>
2023-04-24 21:42:44 -0600

    mvp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,7 +37,7 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-python experience-icon"></i>}
+            icon={<i className="fas fa-graduation-cap mx-auto experience-icon"></i>}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
